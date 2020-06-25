bd1ddeacb3e194c4b2685ff3b6b0101c5c633cb3
lex-marie790 <lexi.marie790@gmail.com>
2020-06-24 17:24:24 -0700

    project setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import './App.css';
+
+//components
+import Effects from './Components/Effects';
+import Flavors from './Components/Flavors';
+import Home from './Components/Home';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to my Weed App</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <div className='nav'>
+          <Link to='/'>Home</Link>
+          <Link to='/effects'>Effects</Link>
+          <Link to='/flavors'>Flavors</Link>
+        </div> 
+
+        <Switch>
+          {/* <Route path='/effects' component={Effects} />
+          <Route path='/flavors' component={Flavors} />
+          <Route path='/' component={Home} /> */}
+        </Switch>
+      </div>
+        
+    </Router>
+
   );
 }
 
